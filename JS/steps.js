16614876c27f701c8afa9c2b44f7200cf7fd3a78
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-10-01 22:35:15 -0500

    Defined function with default array param  and takes in 2 other params

--- JS/steps.js
+++ JS/steps.js
@@ -1,1 +1,9 @@
 console.log("Staircase 2!");
+
+// Function takes in 3 params.
+// steps: Amount of steps staircase has
+// max: max amount of steps that can be taken at one time
+// array: array to store results to prevent redundant computation.
+const stairCase = (steps, max, array=[]) => {
+
+}
