386da0bc4757af3038b9a8adbc8c236eb3c40c70
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-10-01 22:36:59 -0500

    When only 0-1 steps, only 1 way to reach top.

--- JS/steps.js
+++ JS/steps.js
@@ -4,6 +4,9 @@ console.log("Staircase 2!");
 // steps: Amount of steps staircase has
 // max: max amount of steps that can be taken at one time
 // array: array to store results to prevent redundant computation.
+// Returns number of ways to reach the top.
 const stairCase = (steps, max, array=[]) => {
+	// When only 0-1 steps, only 1 way to reach top. 
+	if (steps <= 1) return 1;
 
 }
